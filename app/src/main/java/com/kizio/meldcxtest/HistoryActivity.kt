fe720ee04fd172f35b9b58c3a0a6f75e1dc66dfc
Graeme Sutherland <graeme@kizio.com>
2019-07-29 21:08:53 +1000

    Work on the HistoryActivity

--- app/src/main/java/com/kizio/meldcxtest/HistoryActivity.kt
+++ app/src/main/java/com/kizio/meldcxtest/HistoryActivity.kt
@@ -1,7 +1,17 @@
 package com.kizio.meldcxtest
 
+import android.os.Bundle
+import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_history.*
 
 class HistoryActivity : AppCompatActivity() {
 
+	override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+		super.onCreate(savedInstanceState, persistentState)
+
+		setContentView(R.layout.activity_history)
+
+		delegate.setSupportActionBar(history_toolbar)
+	}
 }
